03cb429549622b7b1cd872cb09368e54facfaa2f
siriwatchairak <earchchairuk@gmail.com>
2025-08-16 20:12:30 +0700

    add cors protection

--- stock-analysis-api/app.py
+++ stock-analysis-api/app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://trading-dashboard-ooexw8fu7-algorithm368s-projects.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
